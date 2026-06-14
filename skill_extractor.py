43948521e4e53fb10be42e1aeb4b0cf5cf60f02d
modanishruthi <modanishruthi2006@gmail.com>
2026-06-14 22:26:07 +0530

    Floor 8: Replaced keyword matching with spaCy PhraseMatcher for skill extraction

--- skill_extractor.py
+++ skill_extractor.py
@@ -1,4 +1,7 @@
 import json
+import spacy
+from spacy.matcher import PhraseMatcher
+
 with open("skills_db.json", "r") as f:
     skills=json.load(f)
     
@@ -7,15 +10,32 @@ all_skills=[]
 for key, value in skills.items():
     all_skills.extend(value)
 
+nlp=spacy.load("en_core_web_sm")
+matcher= PhraseMatcher(nlp.vocab, attr="LOWER")
+
+patterns=[nlp.make_doc(s) for s in all_skills]
+matcher.add("SKILLS",patterns )
+
+
 
 def extract_skills(resume_text):
-    resume_text=resume_text.lower()
-    extracted_skills=[]
-    for i in all_skills:
-        if i in resume_text:
-            extracted_skills.append(i)
+    
+    # resume_text=resume_text.lower()
+    # extracted_skills=[]
+    # for i in all_skills:
+    #     if i in resume_text:
+    #         extracted_skills.append(i)
 
-    return list(set(extracted_skills))
+    # return list(set(extracted_skills))
+    doc=nlp(resume_text)
+    matches=matcher(doc)
+
+    extracted=[]
+    for match_id, start, end in matches:
+        span=doc[start:end]
+        extracted.append(span.text.lower())
+
+    return list(set(extracted))
 
 
 def get_skill_gap(resume_text, target_role):
